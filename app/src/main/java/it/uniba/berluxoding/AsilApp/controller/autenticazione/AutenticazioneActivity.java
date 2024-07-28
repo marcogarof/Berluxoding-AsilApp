eb8c9289e0014fc67ab39db99f0c448d4fb160d8
marcogarofalo <marcogarofalo08@gmail.com>
2024-07-28 13:01:57 +0200

    commit

--- app/src/main/java/it/uniba/berluxoding/AsilApp/controller/autenticazione/AutenticazioneActivity.java
+++ app/src/main/java/it/uniba/berluxoding/AsilApp/controller/autenticazione/AutenticazioneActivity.java
@@ -8,15 +8,21 @@ import it.uniba.berluxoding.AsilApp.R;
 import it.uniba.berluxoding.AsilApp.controller.home.HomeActivity;
 
 import android.content.Intent;
+import android.text.TextUtils;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.FrameLayout;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class AutenticazioneActivity extends AppCompatActivity {
 
     private FrameLayout fragmentContainer;
     private LinearLayout loginContainer;
+
+    private EditText etUsername, etPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,8 +32,14 @@ public class AutenticazioneActivity extends AppCompatActivity {
         fragmentContainer = findViewById(R.id.fragment_container);
         loginContainer = findViewById(R.id.login_container);
 
+        etUsername = findViewById(R.id.editTextUsername);
+        etPassword = findViewById(R.id.editTextPassword);
+
         TextView textViewRegister = findViewById(R.id.textViewRegister);
+        Button btnLogin = findViewById(R.id.btnLogin);
+
         textViewRegister.setOnClickListener(v -> openRegistrazioneFragment());
+        btnLogin.setOnClickListener(view -> login());
     }
 
     private void openRegistrazioneFragment() {
@@ -40,7 +52,27 @@ public class AutenticazioneActivity extends AppCompatActivity {
                 .commit();
     }
 
-    public void redirectToMainActivity(View view) {
+    private void login(){
+        String username = etUsername.getText().toString().trim();
+        String password = etPassword.getText().toString().trim();
+
+        if ( TextUtils.isEmpty(username) || TextUtils.isEmpty(password) ) {
+            Toast.makeText(this, "Tutti i campi sono obbligatori", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        //VERIFICARE CHE L'UTENTE ESISTE MEDIANTE QUERY AL DB
+        // e nel caso -> Toast.makeText(this, "Login avvenuto con successo", Toast.LENGTH_SHORT).show();
+        //altrimenti -> Toast.makeText(this, "Credenziali errate", Toast.LENGTH_SHORT).show(); return;
+
+        redirectToHomeActivity();
+    }
+
+    public void redirectToHomeActivity(View view) {
+        redirectToHomeActivity();
+    }
+
+    public void redirectToHomeActivity() {
         Intent intent = new Intent(this, HomeActivity.class);
         startActivity(intent);
         //se si volesse NON dare la possibilità di tornare indietro a questa activity basterebbe chiamare il metodo finish()
--- app/src/main/java/it/uniba/berluxoding/AsilApp/controller/valutazioni/ValutazioniActivity.java
+++ app/src/main/java/it/uniba/berluxoding/AsilApp/controller/valutazioni/ValutazioniActivity.java
@@ -2,14 +2,50 @@ package it.uniba.berluxoding.AsilApp.controller.valutazioni;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RatingBar;
+import android.widget.Toast;
+
 import it.uniba.berluxoding.AsilApp.R;
 
 public class ValutazioniActivity extends AppCompatActivity {
+
+    private RatingBar ratingBarCenter, ratingBarApp;
+    private EditText editTextCenterComments, editTextAppComments;
+    private Button buttonSubmit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_valutazioni);
+
+        // Trova le view nel layout
+        ratingBarCenter = findViewById(R.id.ratingBarCenter);
+        ratingBarApp = findViewById(R.id.ratingBarApp);
+        editTextCenterComments = findViewById(R.id.editTextCenterComments);
+        editTextAppComments = findViewById(R.id.editTextAppComments);
+        buttonSubmit = findViewById(R.id.buttonSubmit);
+
+        // Imposta un listener per il click sul pulsante di invio
+        buttonSubmit.setOnClickListener(v -> salvaRatings());
     }
+
+    private void salvaRatings(){
+
+        // Prendi i valori dalle RatingBar e dagli EditText
+        float centerRating = ratingBarCenter.getRating();
+        String centerComments = editTextCenterComments.getText().toString();
+        float appRating = ratingBarApp.getRating();
+        String appComments = editTextAppComments.getText().toString();
+
+        // Qui puoi gestire i dati raccolti, ad esempio inviarli a un server o salvarli lsu db
+
+        Toast.makeText(this, "Grazie :) !", Toast.LENGTH_LONG).show();
+        finish();
+
+    }
+
 }
 
